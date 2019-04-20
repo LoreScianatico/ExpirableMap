b9eb8569be996b3f6e46f01dd2198d9e4aaad821
Lorenzo <scianaticolorenzo@gmail.com>
2019-04-20 15:09:24 +0200

    Adding empty implementations

---
+++ src/main/java/com/lorescianatico/expirablemap/ConcurrentExpirableMap.java
@@ -0,0 +1,6 @@
+package com.lorescianatico.expirablemap;
+
+import java.util.Map;
+
+public class ConcurrentExpirableMap<K,V> extends ExpirableMap<K,V> implements Map<K,V> {
+}
---
+++ src/test/java/com/lorescianatico/expirablemap/ExpirableMapTest.java
@@ -0,0 +1,100 @@
+package com.lorescianatico.expirablemap;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExpirableMapTest {
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void containsKey() {
+    }
+
+    @Test
+    void containsValue() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void put() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void putAll() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void keySet() {
+    }
+
+    @Test
+    void values() {
+    }
+
+    @Test
+    void entrySet() {
+    }
+
+    @Test
+    void getOrDefault() {
+    }
+
+    @Test
+    void forEach() {
+    }
+
+    @Test
+    void replaceAll() {
+    }
+
+    @Test
+    void putIfAbsent() {
+    }
+
+    @Test
+    void remove1() {
+    }
+
+    @Test
+    void replace() {
+    }
+
+    @Test
+    void replace1() {
+    }
+
+    @Test
+    void computeIfAbsent() {
+    }
+
+    @Test
+    void computeIfPresent() {
+    }
+
+    @Test
+    void compute() {
+    }
+
+    @Test
+    void merge() {
+    }
+}
